ae22ac5c127fbd7e0b6803252b02f89ae634fdc0
dbrud6025 <145435863+dbrud6025@users.noreply.github.com>
2023-10-11 17:00:12 +0900

    Update 약수의 개수와 덧셈.java

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -11,30 +11,21 @@ class Solution {
 	    	for(int i = 1; i <= num/2; i++) {
 	           if(num % i == 0) {
 	            divisor++;
-	            
 	           }
 	        }
-	        
 	        divisorArr[idx] = divisor;
 	        idx++;
 	    }
 
-	for(
+        for(int i = 0;i<right-left+1;i++) {
+            arr[i] = left + i;
+        }
+        for(int i = 0;i<right-left+1;i++) {
+            if (divisorArr[i] % 2 == 0) {
+                answer += arr[i];
+            } else answer -= arr[i];
+        }
 
-	int i = 0;i<right-left+1;i++)
-	{
-		arr[i] = left + i;
-	}
-
-	for(
-	int i = 0;i<right-left+1;i++)
-	{
-		if (divisorArr[i] % 2 == 0) {
-			answer += arr[i];
-		} else
-			answer -= arr[i];
-	}
-
-	return answer;
+        return answer;
     }
 }
