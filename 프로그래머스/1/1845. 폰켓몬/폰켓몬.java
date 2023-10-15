e9b4e83fda0aeb9dd9c42d97fc093106c02147d9
dbrud6025 <145435863+dbrud6025@users.noreply.github.com>
2023-10-15 15:07:22 +0900

    [level 1] Title: 폰켓몬, Time: 2.73 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int kind : nums) {
+            hashSet.add(kind);
+        }
+        if(hashSet.size() >= nums.length / 2) {
+            answer = nums.length / 2;
+        }else answer = hashSet.size();
+        return answer;
+    }
+}
