4c617badb2571347d059ceaeb87716ee033325b3
dbrud6025 <145435863+dbrud6025@users.noreply.github.com>
2023-11-04 23:07:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.73 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder(); //공백을 더해주고 싶어서!
+		String[] str = s.split(" "); //string 배열에 나눠 넣고 list에도 넣기!
+		
+		List<Integer> number = new ArrayList<>();
+		for(int i = 0; i < str.length; i++) {
+			number.add(Integer.parseInt(str[i]));
+		}
+		Collections.sort(number); //오름차순 정렬해서 맨 앞 숫자 + 공백 + 맨 뒤 숫자 (어차피 세 개만 더하니까!)
+		
+		sb.append(Integer.toString(number.get(0)));
+		sb.append(" ");
+		sb.append(Integer.toString(number.get(number.size()-1)));
+		
+		return sb.toString();
+    }
+}
